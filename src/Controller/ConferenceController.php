e00ab605355c7d9575009010c2b9f4dd4cf86187
PJSF\ifvigo <ifvigo@justiciasantafe.gov.ar>
2023-09-26 08:05:12 -0300

    Configure database sessions

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,19 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ConferenceRepository;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
-        );
+        return $this->render('conference/index.html.twig', 
+            [
+                'conferences' => $conferenceRepository->findAll(),
+            ]);
     }
+
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository) : Response
+    {
+       // $comments = $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']);
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig',
+            [
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            ]);
+    }
+
 }
